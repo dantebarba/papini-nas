db74be7b1c93e8dd6df742c7fb7def5c89274280
Dante Barba <dantebarba@gmail.com>
2019-04-28 02:58:01 -0300

    adding env vars (no values set)

---
+++ config/add_env_vars.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo 'DNS_HTTP_USER=' >> /etc/environment
+echo 'DNS_HTTP_PASS=' >> /etc/environment
+echo 'DUPLICATI_HOME=' >> /etc/environment
+echo 'DUPLICATI_CONFIG=' >> /etc/environment
+echo 'RESILIO_CONFIG=' >> /etc/environment
+echo 'DUPLICATI_HOME=' >> /etc/environment
