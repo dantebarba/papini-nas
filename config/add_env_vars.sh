814dafd15b3ceae4425b9038fe398b9ae6c920c0
Dante Barba <dantebarba@gmail.com>
2019-04-28 21:32:04 -0300

    starting firsts test with traefik

--- config/add_env_vars.sh
+++ config/add_env_vars.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 echo 'DNS_HTTP_USER=' >> /etc/environment
 echo 'DNS_HTTP_PASS=' >> /etc/environment
+echo 'DNSMASQ_CONFIG=' >> /etc/environment
 echo 'DUPLICATI_HOME=' >> /etc/environment
 echo 'DUPLICATI_CONFIG=' >> /etc/environment
 echo 'RESILIO_CONFIG=' >> /etc/environment
-echo 'DUPLICATI_HOME=' >> /etc/environment
-echo 'TRAEFIK_HOME=' >> /etc/environment
+echo 'RESILIO_HOME=' >> /etc/environment
+echo 'TRAEFIK_CONFIG=' >> /etc/environment
+echo 'FILEBROWSER_HOME=' >> /etc/environment
+echo 'FILEBROWSER_CONFIG=' >> /etc/environment
