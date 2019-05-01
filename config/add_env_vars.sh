2820a8fe769fdaa0b59424905547d3802d988fe5
Dante Barba <dantebarba@gmail.com>
2019-05-01 15:28:03 -0300

    change rclone config

--- config/add_env_vars.sh
+++ config/add_env_vars.sh
@@ -9,6 +9,7 @@ echo 'RESILIO_HOME=' >> /etc/environment
 echo 'TRAEFIK_CONFIG=' >> /etc/environment
 echo 'FILEBROWSER_HOME=' >> /etc/environment
 echo 'FILEBROWSER_CONFIG=' >> /etc/environment
+echo 'DOCKER_MEDIA_SERVER' >> /etc/environment
 echo 'RCLONE_CONFIG=' >> /etc/environment
 echo 'RADARR_API_TOKEN=' >> /etc/environment
 echo 'SONARR_API_TOKEN=' >> /etc/environment
