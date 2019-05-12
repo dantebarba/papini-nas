5a6d2296e105128f9bf6b5793605ffaf07d98c19
Dante Barba <dantebarba@gmail.com>
2019-05-12 00:13:26 -0300

    file movement

--- scripts/ccleaner.sh
+++ scripts/ccleaner.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# apt and kernel cleaning
 /usr/bin/apt-get autoremove
 /usr/bin/apt-get --purge autoremove
 /usr/bin/apt-get clean
@@ -7,6 +9,8 @@
 /sbin/sysctl -w vm.drop_caches=3
 /bin/sync && /bin/echo 3 | /usr/bin/tee /proc/sys/vm/drop_caches
 
+# logs cleaning
+
 # docker clean up
-/usr/bin/curl --silent https://gist.githubusercontent.com/macropin/3d06cd315a07c9d8530f/raw | bash -s rm-dangling
+/usr/bin/curl --silent https://gist.githubusercontent.com/macropin/3d06cd315a07c9d8530f/raw | /bin/bash -s rm-dangling
 /usr/bin/docker restart $(docker ps -a -q)
