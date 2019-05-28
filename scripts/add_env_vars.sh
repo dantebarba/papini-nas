c3ea8c3bad9c0c05f183005613596024099fb95e
Dante Barba <dantebarba@gmail.com>
2019-05-27 22:26:42 -0300

    quitamos envvars de config, mejor refactoring.

--- scripts/add_env_vars.sh
+++ scripts/add_env_vars.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 echo 'DNS_HTTP_USER=' >> /etc/environment
 echo 'DNS_HTTP_PASS=' >> /etc/environment
+echo 'CONFIG_LOCATION' >> /etc/environment
 echo 'DNSMASQ_CONFIG=' >> /etc/environment
 echo 'DUPLICATI_HOME=' >> /etc/environment
-echo 'DUPLICATI_CONFIG=' >> /etc/environment
-echo 'RESILIO_CONFIG=' >> /etc/environment
 echo 'RESILIO_HOME=' >> /etc/environment
-echo 'TRAEFIK_CONFIG=' >> /etc/environment
 echo 'FILEBROWSER_HOME=' >> /etc/environment
-echo 'FILEBROWSER_CONFIG=' >> /etc/environment
 echo 'DOCKER_MEDIA_SERVER' >> /etc/environment
-echo 'RCLONE_CONFIG=' >> /etc/environment
 echo 'RADARR_API_TOKEN=' >> /etc/environment
 echo 'SONARR_API_TOKEN=' >> /etc/environment
 echo 'REMOTE_UPSTREAM=' >> /etc/environment
 echo 'REMOTE_STORAGE_LOCATION="export $(ssh $REMOTE_UPSTREAM "env | grep STORAGE_LOCATION)"' >> /etc/environment
-echo 'HEALTHCHECK_CONFIG=' >> /etc/environment
-echo 'CLOUDPRINT_CONFIG=' >> /etc/environment
 echo 'CUPS_USER_PASSWORD=' >> /etc/environment
+echo 'DOCKER_LOCATION=' >> /etc/environment
+echo 'LOGIO_ADMIN_PASSWORD=' >> /etc/environment
